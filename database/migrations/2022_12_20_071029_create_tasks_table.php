b64c079cd75628240e65f72192af1d573ffd787b
luthfibg <luthfinewzoneonline@gmail.com>
2022-12-20 15:59:15 +0700

    [SETUP] model task

---
+++ database/migrations/2022_12_20_071029_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('name');
+            $table->string('description');
+            $table->dateTime('deadline', $precision = 0)->nullable();
+            $table->enum('status', ['Not done', 'On progress', 'Done', 'Neglected'])->default('Not done');
+            $table->dateTime('created_at')->useCurrent()->change();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
