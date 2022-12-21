6afac106fc302246ade35d94793d30650f141174
luthfibg <luthfinewzoneonline@gmail.com>
2022-12-21 07:00:02 +0700

    [INIT] bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/preloader_style.css') }}">
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row justify-content-center px-md-5">
+        <div class="col-12 col-md-10">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -19,6 +19,11 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="row justify-content-center px-md-5">
+        <div class="col-12 col-md-4"></div>
+        <div class="col-12 col-md-4"></div>
+        <div class="col-12 col-md-4"></div>
     </div>
 </div>
 @endsection
